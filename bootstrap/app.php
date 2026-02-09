5a120bf0a79e16ce1ce352345e5c3d2cc827058f
Brian Mulawka <mulawkabrian@gmail.com>
2026-02-09 12:22:27 +0100

    activitée 1 corrigé

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,14 +8,13 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-            then: function () {
+        then: function () {
                 Route::middleware('api')
                     ->prefix('api/v1')
                     ->group(base_path('routes/api.php'));
-            }
+        },
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\V1\PostController as PostControllerV1;
 
 
 Route::get('/ping', function () {
 return response()->json([
 'message' => 'pong',
 ]);
-});
-
-Route::prefix('v1')->group(function () {
-Route::apiResource('posts', PostControllerV1::class);
 });
